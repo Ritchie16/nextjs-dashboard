af398189975582c8caa7077f26b6b2a3cc505730
Richard Munthali <richardmunthali016@gmail.com>
2026-01-01 14:40:56 +0200

    update dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 export default function Layout({children}: {children: React.ReactNode}){
     return(
-        <div className='flex border-2 border-green-600 h-screen flex-col md:flex-row md:overflow-hidden'>
-            <div className='w-full border-2 border-red-600 flex-none md:w-64'>
+        <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
+            <div className='w-full flex-none md:w-64'>
                 <SideNav />
             </div>
             <div className='grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
